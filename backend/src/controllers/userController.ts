5fdeda2c43ab04f90c7bfe5e0a707b1d7dbf0dd7
Eduardo Karpfenstein <edukarp10@gmail.com>
2025-09-21 23:41:08 -0300

    feat: encrypt and decrypt users password

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const registerUser = async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
@@ -11,24 +12,28 @@ export const registerUser = async (req: Request, res: Response) => {
       return res.status(400).json({ message: "Email já cadastrado." });
     }
 
-    const newUser = new User({ name, email, password });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({ name, email, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ message: "Usuário cadastrado com sucesso!" });
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: `Erro ao cadastrar usuário.\nDescricao do Erro:\n ${error}`,
-      });
+    res.status(500).json({
+      message: `Erro ao cadastrar usuário.\nDescricao do Erro:\n ${error}`,
+    });
   }
 };
 
 export const loginUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email, password });
+    const user = await User.findOne({ email });
     if (!user) {
+      return res.status(400).json({ message: "Credenciais inválidas." });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
       return res.status(400).json({ message: "Credenciais inválidas." });
     }
 
@@ -36,11 +41,9 @@ export const loginUser = async (req: Request, res: Response) => {
     if (!jwtSecret) {
       throw new Error("JWT_SECRET is not defined in environment variables.");
     }
-    const token = jwt.sign(
-      { userId: user._id, email: user.email },
-      jwtSecret,
-      { expiresIn: "7d" }
-    );
+    const token = jwt.sign({ userId: user._id, email: user.email }, jwtSecret, {
+      expiresIn: "7d",
+    });
 
     res.status(200).json({ message: "Login bem-sucedido!", token });
   } catch (error) {
