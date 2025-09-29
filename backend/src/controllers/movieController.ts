21ed785ba0ef54b4a1663816c9f5a586fdedab0d
Eduardo Karpfenstein <edukarp10@gmail.com>
2025-09-29 16:39:07 -0300

    fix: create movies bug

--- backend/src/controllers/movieController.ts
+++ backend/src/controllers/movieController.ts
@@ -8,6 +8,7 @@ export const createMovie = async (req: Request, res: Response) => {
     await movie.save();
     res.status(201).json(movie);
   } catch (error) {
+    console.error('Erro ao criar filme/série:', error);
     res.status(500).json({ message: "Erro ao criar filme/série.", error });
   }
 };
@@ -25,7 +26,7 @@ export const getAllMovies = async (_req: Request, res: Response) => {
 // Buscar filme/série por ID
 export const getMovieById = async (req: Request, res: Response) => {
   try {
-    const movie = await Movie.findOne({ id: req.params.id });
+    const movie = await Movie.findOne({ id: req.params._id });
     if (!movie)
       return res.status(404).json({ message: "Filme/série não encontrado." });
     res.json(movie);
@@ -72,7 +73,7 @@ export const getBestRatedMovies = async (_req: Request, res: Response) => {
 export const updateMovie = async (req: Request, res: Response) => {
   try {
     const movie = await Movie.findOneAndUpdate(
-      { id: req.params.id },
+      { id: req.params._id },
       req.body,
       { new: true }
     );
@@ -87,7 +88,7 @@ export const updateMovie = async (req: Request, res: Response) => {
 // Remover filme/série
 export const deleteMovie = async (req: Request, res: Response) => {
   try {
-    const movie = await Movie.findOneAndDelete({ id: req.params.id });
+    const movie = await Movie.findOneAndDelete({ id: req.params._id });
     if (!movie)
       return res.status(404).json({ message: "Filme/série não encontrado." });
     res.json({ message: "Filme/série removido com sucesso." });
